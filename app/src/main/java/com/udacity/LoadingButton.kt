3a050581873c02fad4aa41ea7284a003c0c9d0dd
Melvyn Drag <melvyniandrag@gmail.com>
2023-06-13 03:49:44 -0400

    code refactor. Still not 100% happy with the logic, but must go to bed now.

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -4,29 +4,28 @@ import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
-import java.lang.Math.ceil
-import java.lang.Math.round
 import kotlin.math.roundToInt
 import kotlin.properties.Delegates
 
+private const val LOADING_ANIMATION_DURATION = 10000L
+private const val LOADING_ARC_RADIUS = 50.0f
+private const val LOADING_ARC_TRANSLATION_RIGHT = 80.0f
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    // Question! - Why do we save these? Why can't we just use width and height?
     private var widthSize = 0
     private var heightSize = 0
 
-    //private val valueAnimator = ValueAnimator()
     private val valueAnimator = ValueAnimator.ofFloat(0.0f, 1.0f)
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
 
-    private var animationProgress = 0.0f
-
+    private var animationProgressPercent = 0.0f
 
     private val loadingPaint = Paint().apply {
         color = resources.getColor(R.color.colorPrimaryDark, null)
@@ -51,28 +50,43 @@ class LoadingButton @JvmOverloads constructor(
 
     private val loadingCirclePaint = Paint().apply {
         color = resources.getColor(R.color.colorAccent, null)
-        // Smooths out edges of what is drawn without affecting shape.
+        strokeWidth = 0.0f
         isAntiAlias = true
-        // Dithering affects how colors with higher-precision than the device are down-sampled.
-        isDither = true
+        strokeCap = Paint.Cap.ROUND
         style = Paint.Style.FILL_AND_STROKE // default: FILL
     }
 
-    private var loadingRectWidth = 0
+    private var loadingRect : Rect = Rect(0, 0, (widthSize * animationProgressPercent).roundToInt(), heightSize )
+    private val textWidth = loadingTextPaint.measureText(resources.getString(R.string.button_loading))
+    private var loadingArcRect = RectF(
+        widthSize / 2.0f - LOADING_ARC_RADIUS + textWidth/2.0f  + LOADING_ARC_TRANSLATION_RIGHT,
+        heightSize/2.0f - LOADING_ARC_RADIUS,
+        widthSize / 2.0f + LOADING_ARC_RADIUS + textWidth/2.0f + LOADING_ARC_TRANSLATION_RIGHT,
+        heightSize/2.0f + LOADING_ARC_RADIUS
+    )
 
     init {
-        isClickable = true
-        valueAnimator.setDuration(2000)
+        isClickable = true // wow, I lost a half hour trying to figure out why I couldnt click my button!!
+
+        valueAnimator.duration = LOADING_ANIMATION_DURATION
 
         valueAnimator.addUpdateListener { animation ->
-            animationProgress = (animation.animatedValue as Float)
-            loadingRectWidth = (width * animationProgress).roundToInt()
+            animationProgressPercent = (animation.animatedValue as Float)
+            loadingRect = Rect(0, 0, (widthSize * animationProgressPercent).roundToInt(), heightSize )
             invalidate()
         }
+
     }
 
     override fun performClick(): Boolean {
         super.performClick()
+        loadingRect = Rect(0, 0, (widthSize * animationProgressPercent).roundToInt(), heightSize )
+        loadingArcRect = RectF(
+            widthSize / 2.0f - LOADING_ARC_RADIUS + textWidth/2.0f  + LOADING_ARC_TRANSLATION_RIGHT,
+            heightSize/2.0f - LOADING_ARC_RADIUS,
+            widthSize / 2.0f + LOADING_ARC_RADIUS + textWidth/2.0f + LOADING_ARC_TRANSLATION_RIGHT,
+            heightSize/2.0f + LOADING_ARC_RADIUS
+        )
         valueAnimator.start()
         return true
     }
@@ -80,26 +94,9 @@ class LoadingButton @JvmOverloads constructor(
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         canvas?.drawColor(resources.getColor(R.color.colorPrimary, null))
-        //Log.e("hello", (width * animationProgress).toString())
-
-        val loadingRect = Rect(0, 0, loadingRectWidth, height )
         canvas?.drawRect(loadingRect, loadingPaint)
-        canvas?.drawText("We are loading", width/2.0f, height * 0.65f, loadingTextPaint)
-        //canvas?.drawCircle(width * 0.5f + 550.0f, height * 0.5f, 50f, loadingCirclePaint)
-        val textWidth = loadingTextPaint.measureText("We are loading")
-        val paint = Paint()
-        val rect = RectF(width / 2.0f - 50.0f + textWidth/2.0f  + 80.0f, height/2.0f - 50.0f, width / 2.0f + 50.0f + textWidth/2.0f + 80.0f, height/2.0f + 50.0f)
-
-        //Example values
-        //Example values
-
-        paint.color = resources.getColor(R.color.colorAccent, null)
-        paint.strokeWidth = 0.0f
-        paint.isAntiAlias = true
-        paint.strokeCap = Paint.Cap.ROUND
-        paint.style = Paint.Style.FILL_AND_STROKE
-        canvas!!.drawArc(rect, 0.0f, (animationProgress * 360.0f), true, paint)
-
+        canvas?.drawText(resources.getString(R.string.button_loading), width/2.0f, height * 0.65f, loadingTextPaint)
+        canvas?.drawArc(loadingArcRect, 0.0f, (animationProgressPercent * 360.0f), true, loadingCirclePaint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -113,6 +110,7 @@ class LoadingButton @JvmOverloads constructor(
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
+
     }
 
 }
