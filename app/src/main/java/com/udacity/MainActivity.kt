34123e70f491c6e2a580ca7d7123e14b2ab4ff69
Melvyn Drag <melvyniandrag@gmail.com>
2023-06-15 02:52:17 -0400

    Able to send notifications!

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.udacity
 
+import android.annotation.SuppressLint
 import android.app.DownloadManager
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.database.Cursor
+import android.graphics.Color
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import com.udacity.databinding.ActivityMainBinding
+import com.udacity.util.sendNotification
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,31 +44,58 @@ class MainActivity : AppCompatActivity() {
 
         // learned viewBinding from here https://www.youtube.com/watch?v=z0F2QTAKsWU
         // hope to be as good as Philipp Lackner in Android. He is very knowledgeable about so many aspects of Android development.
+        // also I see here what was described in the previous lecture. LoadingButton has default behavior in performClick() and
+        // we add additional behaviors by adding a click listener, which is called inside performClick().
         binding.contentMainLayout.customButton.setOnClickListener {
             download()
         }
+
+        createChannel(
+            getString(R.string.detail_notification_channel_id),
+            getString(R.string.detail_notification_channel_name)
+        )
     }
 
+    private fun createChannel(channelId: String, channelName: String) {
+        // TODO: Step 1.6 START create a channel
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            Log.i("Notification", "Notification channel being created")
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+                .apply{
+                    setShowBadge(false)
+                }
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Show download result details"
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+            Log.i("Notification", "Notification channel was created")
+        }
+    }
+
+
     fun onRadioButtonClicked(view: View){
-
         if (view is RadioButton) {
-            // Is the button now checked?
             val checked = view.isChecked
-
-            // Check which radio button was clicked
             when (view.getId()) {
                 R.id.radio_button_glide ->
                     if (checked) {
-                        // Pirates are the best
-
+                        url = "https://github.com/bumptech/glide"
                     }
                 R.id.radio_button_starter ->
                     if (checked) {
-                        // Ninjas rule
+                        url = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
                     }
                 R.id.radio_button_retrofit ->
                     if (checked) {
-                        // Ninjas rule
+                        url = "https://github.com/square/retrofit"
                     }
             }
         }
@@ -68,9 +103,46 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    //Learned about this from Philipp Lackner's youtube channel.
+    // https://www.youtube.com/watch?v=4t8EevQSYK4
+    // Across the internet and in his video many people create a class to extend BroadCastReceiver() and then they
+    // register it in the manifest. Looks like here we dont need to register it!  Why?
+    // Nevermind, I see it is registered iwth a call to registerReceiver()
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if(id == downloadID){ // then this broadcast was about the file we were trying to download just now.
+                val query = DownloadManager.Query()
+                query.setFilterById(id)
+                val manager = context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+                val cursor = manager.query(query)
+                if (cursor.moveToFirst()) {
+                    val colIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS)
+                    val status = cursor.getInt(colIndex)
+                    if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                        val notificationManager = ContextCompat.getSystemService(
+                            context,
+                            NotificationManager::class.java
+                        ) as NotificationManager
+
+                        notificationManager.sendNotification(
+                            "Success",
+                            context
+                        )
+                    } else {
+                        val notificationManager = ContextCompat.getSystemService(
+                            context,
+                            NotificationManager::class.java
+                        ) as NotificationManager
+
+                        notificationManager.sendNotification(
+                            "Fail",
+                            context
+                        )
+                    }
+                }
+                cursor.close()
+            }
         }
     }
 
@@ -89,8 +161,7 @@ class MainActivity : AppCompatActivity() {
                 .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        downloadID = downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
     companion object {
